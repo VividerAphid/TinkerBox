4e803dbca1ebc1d6086fb59ba57d407f223dc76b
VividerAphid <trever.kress@gmail.com>
2021-08-29 23:17:04 -0400

    Basic layout for player

--- proj/BetaSkillsAndCrafts/classes.js
+++ proj/BetaSkillsAndCrafts/classes.js
@@ -23,7 +23,46 @@ class skill{
 }
 
 class player{
+    constructor(id, name){
+        this.id = id;
+        this.name = name;
+        this.skills = this.initSkills();
+        this.health = 100;
+        this.inventory = {slots: 10};
+    }
 
+    initSkills(){
+        return {
+            analysis: new skill("Analysis"),
+
+            tool: new skill("Tool"),
+            pickaxe: new skill("Pickaxe"),
+            hammer: new skill("Hammer"),
+            shovel: new skill("Shovel"),
+            axe: new skill("Axe"),
+            hoe: new skill("Hoe"),
+            bag: new skill("Bag"),
+
+            athletics: new skill("Athletics"),
+            strength: new skill("strength"),
+            running: new skill("Running"),
+            jumping: new skill("Jumping"),
+
+            weapon: new skill("Weapon"),
+            sword: new skill("Sword"),
+            dagger: new skill("Dagger"),
+            club: new skill("Club"),
+            bow: new skill("Bow"),
+            crossbow: new skill("Crossbow"),
+            sling: new skill("Sling"),
+            firearm: new skill("Firearm"),
+
+            mining: new skill("Mining"),
+            gathering: new skill("Gathering"),
+            farming: new skill("Farming"),
+            butchering: new skill("Butchering")
+        };
+    }
 }
 
 class item{
