a5174fa98829d76ad7d0f08c86b3d8fabb96d794
VividerAphid <trever.kress@gmail.com>
2021-11-01 21:42:08 -0400

    Added colour mutation in biomeColours

--- proj/biomeColours/biomeColours.js
+++ proj/biomeColours/biomeColours.js
@@ -9,7 +9,7 @@ ctx.fillRect(0, 0, gameCanv.width, gameCanv.height);
 let baseChoice = Math.round(Math.random()*(baseList.length-1));
 console.log(baseList);
 console.log(baseChoice);
-spewColours(250, baseList[baseChoice][0], baseList[baseChoice][1], baseList[baseChoice][2], 30, 0);
+spewColours(250, baseList[baseChoice][0], baseList[baseChoice][1], baseList[baseChoice][2], 30, 40);
 
 function spewColours(count, inRed, inGreen, inBlue, range, mutationRange){
     ctx.fillStyle = "rgb("+inRed + "," +inGreen+","+inBlue+")";
@@ -17,15 +17,29 @@ function spewColours(count, inRed, inGreen, inBlue, range, mutationRange){
     
     for(let r = 0; r<count; r++){
         let variation = Math.round(Math.random()*range);
-        let negative = Math.random();
+        let negativeVari = Math.random();
+
         let redMut = Math.round(Math.random()*mutationRange);
         let greenMut = Math.round(Math.random()*mutationRange);
         let blueMut = Math.round(Math.random()*mutationRange);
+        let negRed = Math.random();
+        let negGreen = Math.random();
+        let negBlue = Math.random();
+
         let x = Math.round(Math.random()*gameCanv.width);
         let y = Math.round(Math.random()*gameCanv.height);
     
-        if(negative > .5){
+        if(negativeVari > .5){
             variation *= -1;
+        }
+        if(negRed > .5){
+            redMut *= -1;
+        }
+        if(negGreen > .5){
+            greenMut *= -1;
+        }
+        if(negBlue > .5){
+            blueMut *= -1;
         }
         ctx.fillStyle = "rgb("+(inRed + variation + redMut) + "," + (inGreen + variation + greenMut) +"," + (inBlue + variation + blueMut) + ")";
         ctx.fillRect(x, y, 25, 25);
