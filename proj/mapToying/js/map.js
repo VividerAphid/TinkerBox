6b107542fd9a9868cfcf42da9feee68f3bcca45e
VividerAphid <trever.kress@gmail.com>
2022-10-15 12:23:15 -0400

    Stable fix for ensuring all planets are connected, working on better alternatives

--- proj/mapToying/js/map.js
+++ proj/mapToying/js/map.js
@@ -59,21 +59,16 @@ function verifyAllConnected(map){
 
     let visitQueue = [map[0]];
     let clusterCount = 0;
-    //let tries = 0;
     while(visitedCount < map.length){
-        //tries++;
-        console.log("New cluster!");
         clusterCount++;
         clusters.push({colour: colList[clusterCount-1], planets: []});
         let currentCluster = clusters[clusterCount-1];
         visitQueue[0].colour = currentCluster.colour;
         visited[visitQueue[0].id] = true;
         visitedCount++;
-        //console.log(visitQueue);
         
         while(visitQueue.length > 0){
             let visiting = visitQueue[0];
-            //console.log(visiting);
             currentCluster.planets.push(visiting);
             for(let r = 0; r < visiting.connections.length; r++){
                 let connectionID = visiting.connections[r];
@@ -90,18 +85,70 @@ function verifyAllConnected(map){
         for(let r = 0; r < visited.length; r++){
             if(visited[r] == false){
                 visitQueue.push(map[r]);
-                //visited[r] = true;
-                //visitedCount++;
                 break;
             }
         }
-        console.log(visitedCount);
-        //console.log(clusters);
 
     }
 
-    console.log(clusters);
-    console.log(visited);
+    if(clusters.length > 1){
+        for(let r = 0; r < clusters.length; r++){
+            clusters[r].averageCoords = {x:0, y:0};
+            let planets = clusters[r].planets;
+            for(let t = 0; t < planets.length; t++){
+                clusters[r].averageCoords.x += planets[t].x;
+                clusters[r].averageCoords.y += planets[t].y;
+            }
+            clusters[r].averageCoords.x = clusters[r].averageCoords.x / planets.length;
+            clusters[r].averageCoords.y = clusters[r].averageCoords.y / planets.length;
+        }
+        console.log(clusters);
+        for(let r = 0; r < clusters.length; r++){
+            let base = clusters[r].averageCoords;
+            let closest;
+            let closestDistance = Infinity;
+            for(let t = 0; t < clusters.length; t++){
+                if(t != r){
+                    let tempTarg = clusters[t].averageCoords;
+                    let tempDist = findLengthPoints(base.x, tempTarg.x, base.y, tempTarg.y);
+                    if(tempDist < closestDistance){
+                        closest = clusters[t];
+                        closestDistance = tempDist;
+                    } 
+                }
+            }
+            let nearSelfPlanet;
+            let nearSelfPlanetDistance = Infinity;
+            for(let x = 0; x < clusters[r].planets.length; x++){
+                let tempX = clusters[r].planets[x].x
+                let tempY = clusters[r].planets[x].y
+                let tempDist = findLengthPoints(tempX, closest.averageCoords.x, tempY, closest.averageCoords.y);
+                if(tempDist < nearSelfPlanetDistance){
+                    nearSelfPlanet = clusters[r].planets[x];
+                    nearSelfPlanetDistance = tempDist;
+                }
+            }
+            nearSelfPlanet.colour = "#555";
+
+            let nearTargPlanet;
+            let nearTargPlanetDistance = Infinity;
+            console.log(closest.planets.length);
+            for(let x = 0; x < closest.planets.length; x++){
+                let tempX = closest.planets[x].x
+                let tempY = closest.planets[x].y
+                let tempDist = findLengthPoints(tempX, nearSelfPlanet.x, tempY, nearSelfPlanet.y);
+                if(tempDist < nearTargPlanetDistance){
+                    nearTargPlanet = closest.planets[x];
+                    nearTargPlanetDistance = tempDist;
+                }
+            }
+            nearTargPlanet.colour = "#fff";
+
+            nearTargPlanet.connections.push(nearSelfPlanet.id);
+            nearSelfPlanet.connections.push(nearTargPlanet.id);
+        }
+        map = verifyAllConnected(map);
+    }
 
     return map;
 }
