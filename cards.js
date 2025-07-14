9a3bd023961392ab9c0257f6e7d5dbe4bf015f32
VividerAphid <trever.kress@gmail.com>
2025-07-14 14:25:26 -0400

    Additions for new cards and pictures

--- cards.js
+++ cards.js
@@ -8,6 +8,8 @@ let projectsInfo = [
     {title:"Raffle", colour: "#2200aa", description: "I made a simple raffle ticket drawing thing", image: '<img class="projectImg" src="projectIcons/raffle.PNG" alt="Picture was here...">', linkAddress:'<a href="proj/raffle/raffle.html" target="_blank">Link here!</a>'},
     {title:"Plant", colour: "#22dd11", description: "I fiddled with a very derpy plant growth project", image: '<img class="projectImg" src="projectIcons/plant.PNG" alt="Picture was here...">', linkAddress:'<a href="proj/plant/plant.html" target="_blank">Link here!</a>'},
     {title:"Cookies", colour: "#008888", description: "I made a tiny page to mess with saving cookies", image: '<img class="projectImg" src="" alt="Picture was here...">', linkAddress:'<a href="proj/cookies/cookies.html" target="_blank">Link here!</a>'},
+    {title:"Flowers", colour: "#cc54c8", description: "An extension of <a href='proj/plant/plant.html' target='_blank'>plant</a>, I wanted the flowers to 'evolve' over time by changing colors and sizes.", image: '<img class="projectImg" src="projectIcons/flowers.png" alt="Picture was here...">', linkAddress:'<a href="proj/flowers/flowers.html" target="_blank">Link here!</a>'},
+    {title:"Screensavers", colour: "#eb4034", description: "I started making a flower 'evolution' project, then got more ideas for screensavers", image: '<img class="projectImg" src="projectIcons/Screensavers.png" alt="Picture was here...">', linkAddress:'<a href="proj/screensavers/screensavers.html" target="_blank">Link here!</a>'},
     ];
 
 let deck = [];
